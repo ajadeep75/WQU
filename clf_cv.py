a73cc74f820866a66de63f4ce0902f9d0a49b77a
Ajadeep Sai <86913983+ajadeep75@users.noreply.github.com>
2022-12-19 12:45:55 +0530

    Update clf_cv.py

--- clf_cv.py
+++ clf_cv.py
@@ -6,3 +6,6 @@ clf = make_pipeline(SimpleImputer(), RandomForestClassifier(random_state=42))
 # cross validation
 cv_scores = cross_val_score(clf, X_train_over, y_train_over, cv=5, n_jobs=-1)
 print(cv_scores)
+
+class_report = classification_report(y_test, model.predict(X_test))
+print(class_report)
