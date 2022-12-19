b0ead0f241d361ce0752d475c6a20e52d68cfd28
Ajadeep Sai <86913983+ajadeep75@users.noreply.github.com>
2022-12-19 12:06:37 +0530

    Create cutoff.py

---
+++ cutoff.py
@@ -0,0 +1,9 @@
+cutoff_test = int(len(y) * <percentage>)
+y_train = y.iloc[:cutoff_test]
+y_test = y.iloc[cutoff_test:]
+print("y_train shape:", y_train.shape)
+print("y_test shape:", y_test.shape)
+
+# sample output
+y_train shape: (1533,)
+y_test shape: (171,)
